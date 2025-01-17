ad8e2164c2e25961f5ccadcb7380d81cd1022a6f
MartinStropp <ma1.stropp@gmail.com>
2025-01-17 02:05:02 +0100

    final final final

--- PaperlessRest/src/main/java/at/technikumwien/paperless/rest/service/DocumentService.java
+++ PaperlessRest/src/main/java/at/technikumwien/paperless/rest/service/DocumentService.java
@@ -2,23 +2,15 @@ package at.technikumwien.paperless.rest.service;
 
 
 import at.technikumwien.paperless.rest.repository.jpa.DocumentJpa;
-import at.technikumwien.paperless.rest.DocumentController;
 
 
 import at.technikumwien.paperless.rest.repository.jpa.DocumentJpaRepository;
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.PutObjectArgs;
-import io.minio.errors.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.stream.Collectors;
 

5c1149fda1db286045c80f134863568d8ba406d8
MartinStropp <ma1.stropp@gmail.com>
2025-01-17 02:05:58 +0100

    final final final

--- PaperlessOCRWorker/src/main/java/at/technikum/paperless/ocrworker/PaperlessOcrWorkerApplication.java
+++ PaperlessOCRWorker/src/main/java/at/technikum/paperless/ocrworker/PaperlessOcrWorkerApplication.java
@@ -1,7 +1,5 @@
 package at.technikum.paperless.ocrworker;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
